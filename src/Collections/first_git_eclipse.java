2542c59925734598c9a3c1aa4d6c6f9f6e81d1d6
hyun kyung <gusl108@naver.com>
2019-05-25 19:55:00 +0900

    ggit_eclipst test

---
+++ src/Collections/first_git_eclipse.java
@@ -0,0 +1,10 @@
+package Collections;
+
+public class first_git_eclipse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("git_eclipst test");
+	}
+
+}
