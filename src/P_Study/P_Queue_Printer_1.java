87ba2c5a88c3e3ee44a49506ebdeb721127dcbcf
hyun kyung <gusl108@naver.com>
2019-06-01 20:51:07 +0900

    과정1

--- src/P_Study/P_Queue_Printer_1.java
+++ src/P_Study/P_Queue_Printer_1.java
@@ -1,12 +1,23 @@
 package P_Study;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+
+class Print {
+	int num;
+	int prior;
+
+	Print(int num, int prior) {
+		this.num = num;
+		this.prior = prior;
+	}
+}
 
 public class P_Queue_Printer_1 {
 	public static void main(String[] args) {
-		int[] priorities = new int[] { 2, 1, 3, 2 };
+//		int[] priorities = new int[] { 2, 1, 3, 2 };
 //	      int location = 2;
-//		int[] priorities = new int[] { 1, 1, 9, 1, 1, 1 };
+		int[] priorities = new int[] { 1, 1, 9, 1, 1, 1 };
 		int location = 0;
 		System.out.println(solution(priorities, location));
 		// return 1이 나와야함
@@ -14,41 +25,41 @@ public class P_Queue_Printer_1 {
 	}
 
 	public static int solution(int[] priorities, int location) {
-		LinkedList<Integer> qu = new LinkedList<Integer>();
-		LinkedList<Integer> loc = new LinkedList<Integer>();
-		LinkedList<Integer> result = new LinkedList<Integer>();
-		for (int i = 0; i < priorities.length; i++) {
-			qu.add(priorities[i]); // 2,1,3,2 //3,2,2,1
-			loc.add(i); // 0,1,2,3 //2,3,0,1
-		}
-//			System.out.println(qu);  
-//			System.out.println(loc);  
-		while (qu.size() > 0) {
+		int answer = 0;
 
-			int max = qu.get(0);
-			int flag = 0;
-			for (int i = 1; i < qu.size(); i++) {
-				if (qu.get(i) > max) {
-					flag = 1; // 나보다 큰놈이 있다면! flag=1
+		LinkedList<Print> ll = new LinkedList<>();
+
+		for (int i = 0; i < priorities.length; i++) {
+			ll.add(new Print(i, priorities[i]));
+		}
+
+		Print first = null;
+		boolean b = false;
+		ArrayList<Integer> idx = new ArrayList<>();
+		
+		while (ll.size()>0) {
+			first = ll.peek();
+			for (int i = 1; i < ll.size(); i++) {
+				if (first.prior < ll.get(i).prior) {
+					b = true;
 				}
 			}
-			if (flag == 1) { // 나보다 큰놈이 있으므로 뒤로보내기
-				qu.add(qu.peek());
-				loc.add(loc.peek());
-				qu.remove();
-				loc.remove();
-			} else { // 나보다 큰놈이 없으므로 출력하기
-				qu.remove();
-				result.add(loc.poll());
-			}
 
+			if (b) {
+				ll.removeFirst();
+				ll.add(first);
+			} else {
+				idx.add(first.num+1);
+				ll.removeFirst();
+			}
+			
+			b=false;
 		}
-		// 하나 남음
-		int answer = 1;
-		for (int i = 0; location != result.get(i); i++) {
-			answer++;
-		}
+
+		System.out.println(idx);
+		
+		answer=idx.get(location);
+		
 		return answer;
 	}
 }
-
