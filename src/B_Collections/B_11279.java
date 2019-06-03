51b79441cf9d6dc2d94cd058b1ad33173dfd8802
hyun kyung <gusl108@naver.com>
2019-06-03 21:44:16 +0900

    comparator 이용
    우선순위큐 이용하고 comparator implements 하여 해결

--- src/B_Collections/B_11279.java
+++ src/B_Collections/B_11279.java
@@ -1,30 +1,41 @@
 package B_Collections;
 
+import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class B_11279 {
 
-	public static void main(String[] args) {
-		PriorityQueue<Integer> qu = new PriorityQueue<>();
-		Scanner sc = new Scanner(System.in);
-
-		int size = sc.nextInt();
-		int num = 0;
-
-		while (size-- > 0) {
-			num = sc.nextInt();
-			if (num == 0) {
-				if (qu.isEmpty()) {
-					System.out.println(0);
-				} else {
-					System.out.println(qu.poll());
-				}
-			} else
-				qu.offer(num);
-		}
-
-		sc.close();
-	}
+   
+   static class Compare implements Comparator<Integer>{
+      
+      @Override
+      public int compare(Integer o1, Integer o2) {
+         return o2.compareTo(o1);
+      }
+      
+   }
+   
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      PriorityQueue<Integer> pq = new PriorityQueue<>(new Compare());
+      
+      
+      int size = sc.nextInt();
+      int value =0;
+      
+      while(size -- >0) {
+         value = sc.nextInt();
+         
+         if(value ==0) {
+            if(pq.isEmpty()) System.out.println(0);
+            else System.out.println(pq.poll());
+         }
+         else {
+            pq.offer(value);
+         }
+      }
+      sc.close();
+   }
 
 }
