15ee764b96cc1125e8523374c70f437b8f02ba49
hyun kyung <gusl108@naver.com>
2019-06-03 23:53:45 +0900

    Create P_Sort_TheGreatestNumber.java
    재귀로 풀어야될거같다

---
+++ src/P_Study/P_Sort_TheGreatestNumber.java
@@ -0,0 +1,38 @@
+package P_Study;
+
+import java.util.LinkedList;
+
+
+
+class Solution {
+    public String solution(int[] numbers) {
+        String answer = "";
+        
+        
+        
+        LinkedList<Integer> list = new LinkedList<>();
+        for(int i=0;i<numbers.length;i++) {
+        	list.add(numbers[i]);
+        }
+        
+        int first=0;
+       while(true) {
+    	   first = list.peek();
+    	   
+       }
+        
+        return answer;
+    }
+    
+    LinkedList<Integer> cycle(LinkedList<Integer> ll) {
+    	LinkedList<Integer> ll1 = new LinkedList<>();
+    	return ll1;
+    }
+    
+}
+public class P_Sort_TheGreatestNumber {
+	public static void main(String[] args) {
+		
+	}
+	
+}
