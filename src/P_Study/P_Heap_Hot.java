81c30334e38cb5b573b578fd7ae9f5b317dbaa3d
hyun kyung <gusl108@naver.com>
2019-06-13 22:04:52 +0900

    Update P_Heap_Hot.java
    조건하나 빠트린거 해결

--- src/P_Study/P_Heap_Hot.java
+++ src/P_Study/P_Heap_Hot.java
@@ -5,32 +5,35 @@ import java.util.PriorityQueue;
 public class P_Heap_Hot {
 
 	public static void main(String[] args) {
-		int[] scoville = {1, 2, 3, 9, 10, 12};
+		int[] scoville = { 1, 2, 3, 9, 10, 12 };
 		int k = 7;
-		
+
 		System.out.println(solution(scoville, k));
 	}
+
 	public static int solution(int[] scoville, int K) {
-        int answer = 0;
-        PriorityQueue<Integer> qu = new PriorityQueue<>();
-        
-        for(int val : scoville)
-        	qu.offer(val);
-        
-        int first=0,second=0;
-        int cnt =0;
-        	
-        
-        while(qu.peek()<=K) {
-        	first= qu.poll();
-        	second =qu.poll();
-        	
-        	qu.offer(first+second*2);
-        	cnt++;
-        }
-        
-        answer =cnt;
-        
-        return answer;
-    }
+		int answer = 0;
+		PriorityQueue<Integer> heap = new PriorityQueue<>();
+
+		for (int val : scoville)
+			heap.offer(val);
+
+		int first = 0, second = 0;
+		int cnt = 0;
+
+		while (heap.peek() <= K) {
+			if (heap.size() == 1) {
+                return -1;
+            }
+			first = heap.poll();
+			second = heap.poll();
+
+			heap.offer(first + second * 2);
+			cnt++;
+		}
+
+		answer = cnt;
+
+		return answer;
+	}
 }
