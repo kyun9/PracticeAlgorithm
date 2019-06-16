49590dbccf09de16a18ec3bf46de993bc697e8b9
hyun kyung <gusl108@naver.com>
2019-06-16 23:03:52 +0900

    시간초과 해결 방법
    다시보기

--- src/B_Study/B_Stack_1406.java
+++ src/B_Study/B_Stack_1406.java
@@ -1,55 +1,47 @@
 package B_Study;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class B_Stack_1406 {
-	
-	public static void main(String[] args) {
-		Stack<Character> stack= new Stack<>();
-		Stack<Character> stmp= new Stack<>();
-		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
-		int size = sc.nextInt();
-		
-		char[] c =s.toCharArray();
-		for(char i : c) {
-			stack.push(i);
+
+	public static void main(String[] args) throws IOException {
+		Stack<Character> stack = new Stack<>();
+		Stack<Character> stmp = new Stack<>();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		int size = Integer.parseInt(br.readLine());
+		for (int i = 0; i < s.length(); i++) {
+			stack.push(s.charAt(i));
 		}
-		
-		String[] str=null;
-		while(size-- >= 0) {
-			str=sc.nextLine().split(" ");
+		String[] str = null;
+		while (size-- > 0) {
+			str = br.readLine().split(" ");
+			char what = str[0].charAt(0);
 			
-			
-			if(str[0].equals("P")) {
+			if (what == 'P') {
 				stack.push(str[1].charAt(0));
-			}
-			else if(str[0].equals("L")){
-				if(!stack.isEmpty())
+			} else if (what == 'L') {
+				if (!stack.isEmpty())
 					stmp.push(stack.pop());
-			}
-			else if(str[0].equals("D")){
-				if(!stmp.isEmpty())
+			} else if (what == 'D') {
+				if (!stmp.isEmpty())
 					stack.push(stmp.pop());
-			}
-			else if(str[0].equals("B")){
-				if(!stack.isEmpty())
+			} else if (what == 'P') {
+				if (!stack.isEmpty())
 					stack.pop();
 			}
 		}
-		sc.close();
-		
-		
-		if(!stmp.isEmpty()) {
-			while(!stmp.isEmpty()) {
-				stack.push(stmp.pop());
-			}
+
+		while (!stmp.isEmpty()) {
+			stack.push(stmp.pop());
 		}
-		
-		String answer="";
-		for(char a : stack) {
-			answer+=a;
+
+		String answer = "";
+		for (char a : stack) {
+			answer += a;
 		}
 		System.out.println(answer);
 	}
