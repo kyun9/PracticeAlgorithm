b5ef878086b6e4dbad3383da08fdf3a017178a69
hyun kyung <gusl108@naver.com>
2019-06-21 20:19:00 +0900

    왜 안되는 지 모르겠다
    완전히 다른 방법으로 다시해보자

--- src/P_Study/P_BFS_Network.java
+++ src/P_Study/P_BFS_Network.java
@@ -1,90 +1,92 @@
 package P_Study;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 
 public class P_BFS_Network {
-	public static void main(String[] args) {
-		int n=4;
-	//	int[][] computer= {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
-	//	int[][] computer= {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
-	//	int[][] computer= {{1, 1, 0}, {1, 1, 1}, {0, 1, 1}};
-		int[][] computer= {{1, 1, 1,1}, {1, 1, 1,1}, {1, 1, 1,1}};
-		solution(n,computer);
-	}
-	public static int solution(int n, int[][] computers) {
+   public static void main(String[] args) {
+      int n=3;
+   //   int[][] computer= {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
+   //   int[][] computer= {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
+   //   int[][] computer= {{1, 1, 0}, {1, 1, 1}, {0, 1, 1}};
+   //   int[][] computer= {{1, 1, 1,1}, {1, 1, 1,1}, {1, 1, 1,1},{1,1,1,1}};
+   //   int[][] computer= {{1, 1, 0,0}, {1, 1, 0,0}, {0, 0, 1,1},{0, 0, 1,1}};
+      int[][] computer= {{1, 1, 0,0}, {1, 1, 1,0}, {0, 1, 1,0},{0, 0, 0,1}};
+      solution(n,computer);
+   }
+   static ArrayList<ArrayList<Integer>> rel = new ArrayList<>(); 
+   public static int solution(int n, int[][] computers) {
         int answer = 0;
         
         ArrayList<ArrayList<Integer>> list = new ArrayList<>(); 
         
         int size=0;
         for(int i=0;i<n;i++) {
-        	for(int j=i+1;j<n;j++) {
-        		if(computers[i][j]==1) {
-        			list.add( new ArrayList<>() );
-        			list.get(size).add(i);
-        			list.get(size).add(j);
-        			size++;
-        		}
-        	}
+           for(int j=i+1;j<n;j++) {
+              if(computers[i][j]==1) {
+                 list.add( new ArrayList<>() );
+                 list.get(size).add(i);
+                 list.get(size).add(j);
+                 size++;
+              }
+           }
         }
         
-        
+        System.out.println("list :"+list);
+        System.out.println("===============");
+//list에 들지 않는 나머지를 계산해주면되겠찌/
         if(list.isEmpty()) {
-        	answer=n;
+           answer=n;
         }
         else if(list.size()==1) {
-        	answer=n-1;
+           answer=n-1;
         }
         else {
-        	LinkedList<Integer> qu = new LinkedList<>();
-        	int num=-1;
-        	int first=-1;
-        	int last=-1;
-        	while(num++<list.size()) {
-        		if(first==list.get(num).get(0)) {
-        			continue;
-        		}
-        		else {
-        			first=list.get(num).get(0);
-        			last=list.get(num).get(1);
-        			qu.add(first);
-        			qu.add(last);
-        			
-//        			for(int i=0;i<list.size();i++) {
-//        				if(last==list.get(i).get(0)) {
-//        					last=list.get(i).get(1);
-//        					qu.add(last);
-//        				}
-//        			}                                --->이부분 dfs로 들어가야겠다
-        			System.out.println(qu);
-        			if(qu.size()==n) {
-            			answer=1;
-            			break;
-            		}
-        			else {
-        				answer++;
-        				qu.clear();
-        			}
-        		}
-        	}
+           ArrayList<Integer> qu = new ArrayList<>();
+           int num=0;
+           int first=-1;
+           int last=-1;
+           while(num<list.size()) {
+              System.out.println("first : "+first +" size : " + list.size()+ " num : "+num + " list.get(num).get(0) : "+list.get(num).get(0));
+              if(first==list.get(num).get(0)) {
+                 System.out.println("continue");
+                 continue;
+              }
+              else {
+                 first=list.get(num).get(0);
+                 last=list.get(num).get(1);
+                 qu.add(first);
+                 qu.add(last);
+                 
+                 for(int i=0;i<list.size();i++) {
+                    if(last==list.get(i).get(0)) {
+                       last=list.get(i).get(1);
+                       qu.add(last);
+                    }
+                 }                                
+                 System.out.println("qu : "+qu);
+                 
+                 rel.add(new ArrayList<>(qu));
+                 System.out.println("rel : "+rel); 
+                 
+                 if(qu.size()==n) {
+                     answer=1;
+                     break;
+                  }
+                 qu.clear();
+                 num++;
+                 System.out.println("------------");
+              }
+           }
+           answer=rel.size();
         }
         
         
-        
-        
-        for (int i = 0 ; i < list.size() ; i++) { 
-            for (int j = 0 ; j < list.get(i).size() ; j++) { 
-              System.out.print(list.get(i).get(j) + " "); 
-           } 
-           System.out.println(); 
-        } 
         System.out.println("----------------");
         
         System.out.println(answer);
         return answer;
     }
-	
-	
+   
+   
 
 }
