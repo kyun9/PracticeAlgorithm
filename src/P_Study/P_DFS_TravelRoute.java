50547cb5d9aebc50fe01a028ad5a482e9a11b852
hyun kyung <gusl108@naver.com>
2019-07-03 23:55:38 +0900

    Create P_DFS_TravelRoute.java

---
+++ src/P_Study/P_DFS_TravelRoute.java
@@ -0,0 +1,13 @@
+package P_Study;
+
+public class P_DFS_TravelRoute {
+
+	public static void main(String[] args) {
+		String[][] tickets= {{"ICN", "JFK"}, {"HND", "IAD"}, {"JFK", "HND"}};
+		System.out.println(solution(tickets));
+	}
+	public static String[] solution(String[][] tickets) {
+        String[] answer = {};
+        return answer;
+    }
+}
