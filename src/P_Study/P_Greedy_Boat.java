b4b6fcf83037c89af9872c15560fc4a5678bac3f
Hyun Kyung <44720922+kyun9@users.noreply.github.com>
2019-08-14 17:25:02 +0900

    Update P_Greedy_Boat.java

--- src/P_Study/P_Greedy_Boat.java
+++ src/P_Study/P_Greedy_Boat.java
@@ -6,35 +6,14 @@ public class P_Greedy_Boat {
 		System.out.println(solution(new int[]{70, 50, 80, 50},100));
 	}
 	public static int solution(int[] people, int limit) {
-	       int answer = 0;
-
-			Boolean[] b = new Boolean[people.length];
-
-			for (int i = 0; i < b.length; i++) {
-				b[i] = false;
-			}
-
-			int number = 0;
-
-			for (int i = 0; i < people.length; i++) {
-				if (!b[i]) {
-					for (int j = i + 1; j < people.length; j++) {
-						if (people[i] + people[j] <= limit) {
-							if (!b[i] && !b[j]) {
-								b[i] = true;
-								b[j] = true;
-								number++;
-							}
-						}
-					}
-					if (!b[i]&&people[i] <= limit) {
-						b[i] = true;
-						number++;
-					}
-				}
-			}
-
-			answer = number;
-			return answer;
+	       Arrays.sort(people);
+ 
+        int answer = 0;
+        int index = people.length - 1;
+        for(int i = 0; i <= index; i++, answer++) 
+            while(index > i && people[i] + people[index--] > limit) 
+                answer++;
+ 
+        return answer;
 	    }
 }
