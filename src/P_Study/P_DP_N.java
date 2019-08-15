7d585da32d4a794eba2373dde2fddddbf81bcf1e
hyun kyung <gusl108@naver.com>
2019-08-15 23:14:47 +0900

    Create P_DP_N.java

---
+++ src/P_Study/P_DP_N.java
@@ -0,0 +1,13 @@
+package P_Study;
+
+public class P_DP_N {
+
+	public static void main(String[] args) {
+		System.out.println(solution(5,12));
+	}
+
+	public static int solution(int N, int number) {
+		int answer = 0;
+		return answer;
+	}
+}
