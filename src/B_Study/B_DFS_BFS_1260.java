e45d09d553e7d8e337920ce6af67f73a460a4622
hyun kyung <gusl108@naver.com>
2019-08-26 15:33:45 +0900

    인접 리스트를 활용하여 해결

--- src/B_Study/B_DFS_BFS_1260.java
+++ src/B_Study/B_DFS_BFS_1260.java
@@ -1,60 +1,69 @@
 package B_Study;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class B_DFS_BFS_1260 {
-    static ArrayList<Integer>[] a;
-    static boolean[] c;
-    public static void dfs(int x) {
-        if (c[x]) {
-            return;
-        }
-        c[x] = true;
-        System.out.print(x + " ");
-        for (int y : a[x]) {
-            if (c[y] == false) {
-                dfs(y);
-            }
-        }
-    }
-    public static void bfs(int start) {
-        Queue<Integer> q = new LinkedList<Integer>();
-        q.add(start);
-        c[start] = true;
-        while (!q.isEmpty()) {
-            int x = q.remove();
-            System.out.print(x + " ");
-            for (int y : a[x]) {
-                if (c[y] == false) {
-                    c[y] = true;
-                    q.add(y);
-                }
-            }
-        }
-    }
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int start = sc.nextInt();
-        a = (ArrayList<Integer>[]) new ArrayList[n+1];
-        for (int i=1; i<=n; i++) {
-            a[i] = new ArrayList<Integer>();
-        }
-        for (int i=0; i<m; i++) {
-            int u = sc.nextInt();
-            int v = sc.nextInt();
-            a[u].add(v);
-            a[v].add(u);
-        }
-        for (int i=1; i<=n; i++) {
-            Collections.sort(a[i]);
-        }
-        c = new boolean[n+1];
-        dfs(start);
+	static ArrayList<Integer>[] list;
+	static boolean check[];
+
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int v = sc.nextInt();
+		list = (ArrayList<Integer>[]) new ArrayList[n + 1];
+		for (int i = 1; i <= n; i++) {
+			list[i] = new ArrayList<Integer>();
+		}
+		for (int i = 0; i < m; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			list[a].add(b);
+			list[b].add(a);
+		}
+
+		for (int i = 1; i <= n; i++) {
+			Collections.sort(list[i]);
+		}
+		check = new boolean[n + 1];
+		dfs(v);
+		System.out.println();
+		check = new boolean[n+1];
+        bfs(v);
         System.out.println();
-        c = new boolean[n+1];
-        bfs(start);
-        System.out.println();
-    }
+	}
+
+	static void dfs(int v) {
+		if (check[v]) {
+			return;
+		}
+		check[v] = true;
+		System.out.print(v + " ");
+		for (int y : list[v]) {
+			if (check[y] == false)
+				dfs(y);
+		}
+	}
+
+	static void bfs(int v) {
+		Queue<Integer> q = new LinkedList<Integer>();
+		q.add(v);
+		check[v]=true;
+		while(!q.isEmpty()) {
+			int num = q.remove();
+			System.out.print(num + " ");
+			for(int y : list[num]) {
+				if(check[y]==false) {
+					check[y]=true;
+					q.add(y);
+				}
+			}
+		}
+		
+	}
+
 }
