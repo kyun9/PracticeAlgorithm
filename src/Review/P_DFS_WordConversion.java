919b555ae90e9e3308785b43e950329179f8a2b1
hyun kyung <gusl108@naver.com>
2019-08-28 00:01:27 +0900

    프로그래머스 실행 1번 에러

--- src/Review/P_DFS_WordConversion.java
+++ src/Review/P_DFS_WordConversion.java
@@ -1,9 +1,8 @@
 package Review;
 
-import java.util.ArrayList;
 
 public class P_DFS_WordConversion {
-	static int find;
+	static boolean[] check;
 
 	public static void main(String[] args) {
 		String begin = "hit";
@@ -13,41 +12,41 @@ public class P_DFS_WordConversion {
 	}
 
 	public static int solution(String begin, String target, String[] words) {
-		int answer = 0;
-
-		ArrayList<String> list = new ArrayList<String>();
-		for (String s : words)
-			list.add(s);
-
-		if (!list.contains(target))
-			answer = 0;
-
-		dfs(begin, target, words, 0);
-		answer = find;
-		return answer;
-	}
-
-	static void dfs(String begin, String target, String[] words, int depth) {
-
-		for (int a = depth; a < words.length; a++) {
-			int cnt = 0;
-			int tcnt = 0;
-			for (int i = 0; i < words[a].length(); i++) {
-				if (begin.charAt(i) == words[a].charAt(i)) {
-					cnt++;
-				}
-				if (begin.charAt(i) == target.charAt(i)) {
-					tcnt++;
-				}
-			}
-			if (tcnt == 3) {
-				find = depth + 1;
-				return;
-			}
-			if (cnt == 2) {
-				dfs(words[depth], target, words, depth + 1);
-				return;
+		check = new boolean[words.length + 1];
+		for (int i = 0; i < words.length; i++) {
+			if (target.equals(words[i])) {
+				return dfs(begin, target, words);
 			}
 		}
+		return 0;
+	}
+
+	static int dfs(String begin, String target, String[] words) {
+		int answer = 0;
+		for (int i = 0; i < words.length; i++) {
+			if (check[i])
+				continue;
+
+			int cnt = 0;
+			int tcnt = 0;
+			for (int j = 0; j < begin.length(); j++) {
+				if (begin.charAt(j) != target.charAt(j)) {
+					tcnt++;
+				}
+				if (begin.charAt(j) != words[i].charAt(j)) {
+					cnt++;
+				}
+			}
+			if (tcnt == 1) {
+				return 1;
+			}
+			if (cnt == 1) {
+				check[i]=true;
+				answer = 1;
+				answer += dfs(words[i], target, words);
+			}
+
+		}
+		return answer;
 	}
 }
