a0e4fa11b3990951917cba89b02f084d89e9c5f5
hyun kyung <gusl108@naver.com>
2019-08-29 22:00:31 +0900

    Update P_DFS_WordConversion.java

--- src/Review/P_DFS_WordConversion.java
+++ src/Review/P_DFS_WordConversion.java
@@ -3,7 +3,7 @@ package Review;
 
 public class P_DFS_WordConversion {
 	static boolean[] check;
-
+	static int find;
 	public static void main(String[] args) {
 		String begin = "hit";
 		String target = "cog";
@@ -12,41 +12,41 @@ public class P_DFS_WordConversion {
 	}
 
 	public static int solution(String begin, String target, String[] words) {
-		check = new boolean[words.length + 1];
-		for (int i = 0; i < words.length; i++) {
-			if (target.equals(words[i])) {
-				return dfs(begin, target, words);
+		int answer=0;
+		for(int i=0;i<words.length;i++) {
+			if(target.equals(words[i])) {
+				check=new boolean[words.length];
+				dfs(begin,target,words);
 			}
 		}
-		return 0;
+		answer=find;
+		return answer;
 	}
 
-	static int dfs(String begin, String target, String[] words) {
-		int answer = 0;
-		for (int i = 0; i < words.length; i++) {
-			if (check[i])
-				continue;
-
-			int cnt = 0;
-			int tcnt = 0;
-			for (int j = 0; j < begin.length(); j++) {
-				if (begin.charAt(j) != target.charAt(j)) {
+	static void dfs(String begin, String target, String[] words) {
+		for(int i=0;i<words.length;i++) {
+			if(check[i]) continue;
+			
+			int tcnt=0;
+			int cnt=0;
+			for(int j=0;j<begin.length();j++) {
+				if(begin.charAt(j)!=target.charAt(j)) {
 					tcnt++;
 				}
-				if (begin.charAt(j) != words[i].charAt(j)) {
+				if(begin.charAt(j)!=words[i].charAt(j)) {
 					cnt++;
 				}
 			}
-			if (tcnt == 1) {
-				return 1;
+			if(tcnt==1) {
+				find++;
+				return;
 			}
-			if (cnt == 1) {
+			if(cnt==1) {
 				check[i]=true;
-				answer = 1;
-				answer += dfs(words[i], target, words);
+				find++;
+				dfs(words[i],target,words);
+				return;
 			}
-
 		}
-		return answer;
 	}
 }
