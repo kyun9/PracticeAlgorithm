40d27a74dd5099444404b9e22b5a5e9a36cf4a37
hyun kyung <gusl108@naver.com>
2019-10-30 22:57:59 +0900

    Create 타일링.java

---
+++ src/P/타일링.java
@@ -0,0 +1,13 @@
+package P;
+
+public class 타일링 {
+
+	public static void main(String[] args) {
+		System.out.println(solution(4));
+	}
+
+	static public int solution(int n) {
+		int answer = 0;
+		return answer;
+	}
+}
