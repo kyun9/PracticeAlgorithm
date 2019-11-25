0076cd61e9ca6216bda70f8693e397599ef168bb
kyun9 <gusl108@naver.com>
2019-11-25 17:43:11 +0900

    Create S_1249.java

---
+++ src/StudyAlgorithm_SWEA/S_1249.java
@@ -0,0 +1,21 @@
+package StudyAlgorithm_SWEA;
+
+import java.io.*;
+
+public class S_1249 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.valueOf(br.readLine());
+		
+		while(T-->0) {
+			
+			
+			
+			
+			System.out.println("#"+T+" " );
+		}
+		
+	}
+
+}
