4e7ae5edca15943e7cff245dfe11449f5adba360
hyun kyung <gusl108@naver.com>
2019-12-03 22:24:18 +0900

    Update S_1249.java

--- src/StudyAlgorithm_SWEA/S_1249.java
+++ src/StudyAlgorithm_SWEA/S_1249.java
@@ -2,62 +2,59 @@ package StudyAlgorithm_SWEA;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class S_1249 {
 	static int[] dx = { -1, 0, 0, 1 };
 	static int[] dy = { 0, -1, 1, 0 };
-	static boolean[][] check;
-	static int answer;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-//		int T = Integer.parseInt(st.nextToken());
-//		for(int t=1; t<=T; t++) {
-		int size = Integer.valueOf(br.readLine());
-		int[][] arr = new int[size][size];
-		for (int i = 0; i < size; i++) {
-			String[] line = br.readLine().split("");
-			for (int j = 0; j < line.length; j++) {
-				arr[i][j] = Integer.valueOf(line[j]);
-			}
-		}
-		
-		check = new boolean[size][size];
-		answer=999999999;
-		int result = 0;
-		dfs(0, 0, size, arr, result);
+		int T = Integer.parseInt(st.nextToken());
+		for (int t = 1; t <= T; t++) {
+			int size = Integer.valueOf(br.readLine());
+			int[][] maps = new int[size][size];
+			int[][] distance = new int[size][size];
 
-		System.out.println(answer);
-//			System.out.println("#" + t + " " + list.get(0));
-//		}
-
-	}
-
-	static void dfs(int row, int col, int size, int[][] arr, int result) {
-		if (row == size - 1 && col == size - 1) {
-			if(result<answer) {
-				answer=result;
-			}
-			return;
-		}
-		check[row][col] = true;
-		for (int i = 0; i < 4; i++) {
-			int x = row + dx[i];
-			int y = col + dy[i];
-
-			if (x >= 0 && x < size && y >= 0 && y < size) {
-				if (check[x][y] == false) {
-					result += arr[x][y];
-					dfs(x, y, size, arr, result);
-					result -= arr[x][y];
-					check[x][y] = false;
+			for (int i = 0; i < size; i++) {
+				String[] line = br.readLine().split("");
+				Arrays.fill(distance[i], Integer.MAX_VALUE);
+				for (int j = 0; j < line.length; j++) {
+					maps[i][j] = Integer.valueOf(line[j]);
 				}
 			}
+
+			PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
+				@Override
+				public int compare(int[] o1, int[] o2) {
+					return Integer.compare(o1[2], o2[2]);
+				}
+			});
+
+			pq.offer(new int[] { 0, 0, 0 });
+			distance[0][0]=0;
+			while (!pq.isEmpty()) {
+				int[] temp = pq.poll();
+
+				for (int i = 0; i < 4; i++) {
+					int x = temp[0] + dx[i];
+					int y = temp[1] + dy[i];
+
+					if (x >= 0 && x < size && y >= 0 && y < size) {
+						int weight = temp[2] + maps[x][y];
+						if (distance[x][y] > weight) {
+							distance[x][y] = weight;
+							pq.offer(new int[] { x, y, distance[x][y] });
+						}
+					}
+				}
+			}
+			System.out.println("#" + t + " " + distance[size - 1][size - 1]);
 		}
 	}
 
