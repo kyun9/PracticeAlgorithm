b01767c2bcdca4d1344433bfce82b279f862cfc0
kyun9 <gusl108@naver.com>
2020-02-03 18:29:41 +0900

    Create S_4014.java

---
+++ src/StudyAlgorithm_SWEA/S_4014.java
@@ -0,0 +1,116 @@
+package StudyAlgorithm_SWEA;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class S_4014 {
+	static int T, N, X, result;
+	static int[][] map;
+	static ArrayList<String> list;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		T = Integer.parseInt(br.readLine());
+
+		for (int test = 1; test <= T; test++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			X = Integer.parseInt(st.nextToken());
+			map = new int[N][N];
+
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < N; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+
+			result = 0;
+			searchRoad();
+			confirmRoad();
+
+			System.out.println("#" + test + " " + result);
+		}
+	}
+
+	static void searchRoad() {
+		list = new ArrayList<>();
+		String s = "";
+		for (int i = 0; i < N; i++) {
+			s = "";
+			for (int j = 0; j < N; j++) {
+				s += map[i][j];
+			}
+			list.add(s);
+		}
+
+		for (int i = 0; i < N; i++) {
+			s = "";
+			for (int j = 0; j < N; j++) {
+				s += map[j][i];
+			}
+			list.add(s);
+		}
+	}
+
+	static void confirmRoad() {
+		int[] arr = new int[N];
+		while (!list.isEmpty()) {
+			String s = list.remove(0);
+			for (int i = 0; i < s.length(); i++) {
+				arr[i] = s.charAt(i) - '0';
+			}
+
+			int cnt = 0;
+			int temp = arr[0];
+			boolean flag = true;
+			boolean flag2 = false;
+			for (int j = 0; j < N; j++) {
+				if (temp == arr[j]) {
+					cnt++;
+					if (flag2 && cnt == X) {
+						cnt = 0;
+						flag2 = false;
+					}
+				} else if (temp - 1 == arr[j]) {
+					// 앞 칸이 낮아졌다는건 앞쪽에 활주로를 체크해야함
+					if (flag2 && cnt < X) {
+						flag = false;
+						flag2 = false;
+						break;
+					}
+					cnt = 1;
+					flag2 = true;
+					temp = arr[j];
+				} else if (temp + 1 == arr[j]) {
+					if (flag2 && cnt < X) {
+						flag = false;
+						flag2 = false;
+						break;
+					}
+					flag2 = false;
+					// 현 칸이 낮다는건 cnt를 기준으로 활주로를 체크해야함
+					if (cnt < X) {
+						flag = false;
+						break;
+					}
+					cnt = 1;
+					temp = arr[j];
+				} else {
+					// 한 칸 차이가 아니면 활주로 불가
+					flag = false;
+					flag2 = false;
+					break;
+				}
+			}
+			if (flag2)
+				if (cnt < X)
+					flag = false;
+			if (flag)
+				result++;
+		}
+	}
+}
