b78ad17de6896a67c820922afafde063f68d5567
kyun9 <gusl108@naver.com>
2020-10-08 16:02:28 +0900

    Create 프로그래머스_최솟값만들기.java

---
+++ src/B_theSecondHalfOfTheYear/프로그래머스_최솟값만들기.java
@@ -0,0 +1,24 @@
+package B_theSecondHalfOfTheYear;
+
+import java.util.Arrays;
+
+public class 프로그래머스_최솟값만들기 {
+
+	public static void main(String[] args) {
+		int[] a = { 1, 4, 2 };
+		int[] b = { 5, 4, 4 };
+		System.out.println(solution(a, b));
+	}
+
+	static int solution(int[] A, int[] B) {
+		int answer = 0;
+		
+		Arrays.sort(A);
+		Arrays.sort(B);
+
+		for(int i=0;i<A.length;i++) {
+			answer += A[i]* B[B.length-i-1];
+		}
+		return answer;
+	}
+}
