05b5c0078d578828ff72506669a7630df09c31bd
kyun9 <gusl108@naver.com>
2021-01-18 21:39:44 +0900

    Create a1_4.java

---
+++ src/cos_pro/a1_4.java
@@ -0,0 +1,33 @@
+package cos_pro;
+
+import java.util.Arrays;
+
+public class a1_4 {
+	public static  int[] solution(int[] arrA, int[] arrB) {
+		int arrA_idx = 0, arrB_idx = 0;
+        int arrA_len = arrA.length;
+        int arrB_len = arrB.length;
+        int answer[] = new int[arrA_len + arrB_len];
+        int answer_idx = 0;
+        while(arrA_idx<arrA_len && arrB_idx<arrB_len){
+            if(arrA[arrA_idx] < arrB[arrB_idx])
+                answer[answer_idx++] = arrA[arrA_idx++];
+            else
+                answer[answer_idx++] = arrB[arrB_idx++];
+        }
+        while(arrA_idx<arrA_len)
+            answer[answer_idx++] = arrA[arrA_idx++];
+        while(arrB_idx<arrB_len)
+            answer[answer_idx++] = arrB[arrB_idx++];
+        return answer;
+	}
+
+	// The following is main method to output testcase.
+	public static void main(String[] args) {
+		int[] arrA = { -2, 3, 5, 9 };
+		int[] arrB = { 0, 1, 5 };
+		int[] ret = solution(arrA, arrB);
+
+		System.out.println("Solution: return value of the method is " + Arrays.toString(ret) + " .");
+	}
+}
