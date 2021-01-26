e69bf051600112a9d8f816d78ca6328c90812151
kyun9 <gusl108@naver.com>
2021-01-26 22:42:23 +0900

    Create a1_9.java

---
+++ src/cos_pro/a1_9.java
@@ -0,0 +1,33 @@
+package cos_pro;
+
+public class a1_9 {
+	public static String func_a(String str, int len) {
+		String padZero = "";
+		int padSize = len - str.length();
+		for (int i = 0; i < padSize; i++)
+			padZero += "0";
+		return padZero + str;
+	}
+
+	public static int solution(String binaryA, String binaryB) {
+		int maxLength = Math.max(binaryA.length(), binaryB.length());
+		binaryA = func_a(binaryA, maxLength);
+		binaryB = func_a(binaryB, maxLength);
+
+		int hammingDistance = 0;
+		for (int i = 0; i < maxLength; i++)
+			if (binaryA.charAt(i) != binaryB.charAt(i))
+				hammingDistance += 1;
+		return hammingDistance;
+	}
+
+	// The following is main method to output testcase.
+	public static void main(String[] args) {
+		String binaryA = "10010";
+		String binaryB = "110";
+		int ret = solution(binaryA, binaryB);
+
+		// Press Run button to receive output.
+		System.out.println("Solution: return value of the method is " + ret + " .");
+	}
+}
