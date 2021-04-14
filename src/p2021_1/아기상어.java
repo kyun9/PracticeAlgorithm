aa1bdff7f52c0902ab2538ffebbe86431165540a
kyun9 <gusl108@naver.com>
2021-04-15 00:32:32 +0900

    Update 아기상어.java

--- src/p2021_1/아기상어.java
+++ src/p2021_1/아기상어.java
@@ -2,132 +2,125 @@ package p2021_1;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class 아기상어 {
-	static boolean[] visited = new boolean[400];
-	static int[][] map = new int[20][20];
-	static ArrayList<Item> fishes = new ArrayList<Item>();
-	static int[] dx = { -1, 0, 1, 0 };
-	static int[] dy = { 0, -1, 0, 1 };
-	static int N, cnt, targetX, targetY, targetSize = 2, time = 0;
+	static class Node2 implements Comparable<Node2> {
+		int row;
+		int col;
+		int cnt;
+
+		Node2(int row, int col, int cnt) {
+			this.row = row;
+			this.col = col;
+			this.cnt = cnt;
+		}
+
+		@Override
+		public int compareTo(Node2 o) {
+			if (this.row > o.row) {
+				return 1;
+			} else if (this.row == o.row) {
+				if (this.col > o.col)
+					return 1;
+				else
+					return -1;
+			} else {
+				return -1;
+			}
+		}
+
+	}
+
+	public static int N, numOfEat = 0, sharkRow, sharkCol, sharkSize = 2;
+	public static int[][] map;
+	public static int[] dirX = { 0, 0, -1, 1 };
+	public static int[] dirY = { -1, 1, 0, 0 };
+	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static PriorityQueue<Node2> pq = new PriorityQueue<>();
 
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
+		N = Integer.parseInt(br.readLine().trim());
+		pq = new PriorityQueue<Node2>();
+		map = new int[N][N];
 
 		for (int i = 0; i < N; i++) {
-			String[] c = br.readLine().split(" ");
-			int size = c.length;
-
-			for (int j = 0; j < size; j++) {
-				map[i][j] = Integer.parseInt(c[j]);
+			StringTokenizer st = new StringTokenizer(br.readLine().trim());
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
 				if (map[i][j] == 9) {
-					targetX = j;
-					targetY = i;
+					sharkRow = i;
+					sharkCol = j;
 				}
 			}
 		}
+
+		System.out.println(solve());
+	}
+
+	public static int solve() {
+		int time = 0;
 
 		while (true) {
-			Queue<Item> q = new LinkedList<Item>();
-			ArrayList<Item> fishes = new ArrayList<Item>();
-			visited = new boolean[400];
+			searchFish();
+			if (pq.isEmpty()) {
+				return time;
+			}
 
-			q.add(new Item(targetX, targetY, 0));
-			visited[targetY * N + targetX] = true;
+			Node2 fish = pq.poll();
+			time += fish.cnt;
+			map[sharkRow][sharkCol] = 0;
+			sharkRow = fish.row;
+			sharkRow = fish.col;
+			numOfEat += 1;
 
-			int found = -1;
-			while (!q.isEmpty()) {
-				Item item = q.poll();
-				int r = item.n / N;
-				int c = item.n % N;
-				int move = item.move;
-				if (found == move) {
-					break;
-				}
+			if (numOfEat == sharkSize) {
+				sharkSize += 1;
+				numOfEat = 0;
+			}
 
-				for (int k = 0; k < 4; k++) {
-					int nx = c + dx[k];
-					int ny = r + dy[k];
-					int next = ny * N + nx;
+			pq.clear();
 
-					if (0 <= nx && nx < N && 0 <= ny && ny < N) {
-						if (visited[next]) {
-							continue;
-						}
-						visited[next] = true;
-						if (targetSize >= map[ny][nx]) {
-							if (map[ny][nx] > 0 && targetSize > map[ny][nx]) {
-								// 물고기를 먹을 수 있는 곳을 찾음. 현재 이동거리까지만 BFS 탐색하고 중단.
-								found = move + 1;
-								fishes.add(new Item(nx, ny, move + 1));
-							}
-							q.add(new Item(nx, ny, move + 1));
-						}
+		}
+	}
+
+	public static void searchFish() {
+		Queue<Node2> q = new LinkedList<>();
+		boolean[][] visited = new boolean[N][N];
+		int dist = Integer.MAX_VALUE;
+
+		q.offer(new Node2(sharkRow, sharkCol, 0));
+		visited[sharkRow][sharkCol] = true;
+
+		while (!q.isEmpty()) {
+			Node2 node = q.poll();
+			int row = node.row;
+			int col = node.col;
+			int cnt = node.cnt;
+
+			for (int i = 0; i < 4; i++) {
+				int nr = row + dirX[i];
+				int nc = col + dirY[i];
+
+				if (isBoundary(nr, nc) && !visited[nr][nc] && cnt < dist && map[nr][nc] <= sharkSize) {
+					if (map[nr][nc] > 0 && map[nr][nc] < sharkSize) {
+						dist = Math.min(dist, cnt + 1);
+						pq.offer(new Node2(nr, nc, cnt + 1));
+						visited[nr][nc] = true;
+					} else if (map[nr][nc] == 0 && map[nr][nc] == sharkSize) {
+						q.offer(new Node2(nr, nc, cnt + 1));
+						visited[nr][nc] = true;
 					}
 				}
-			}
 
-			// 큐에 남은 것들은 먹을 수 있는 거리에 있는 영역들(크기가 동일하거나 작은 물고기 존재)
-			// 먹을 물고기 있다면, 상어 위치 이동 후, 다시 시작.
-			if (found == -1) {
-				break;
-			} else {
-				if (fishes.size() > 1) {
-					Collections.sort(fishes, new ySort());
-				}
-			}
-
-			Item fish = fishes.get(0);
-			if (found != -1) {
-				time += found;
-				map[targetY][targetX] = 0;
-				targetX = fish.x;
-				targetY = fish.y;
-				map[targetY][targetX] = 9;
-				if (targetSize == ++cnt) {
-					targetSize++;
-					cnt = 0;
-				}
-			}
-
-		}
-		System.out.println(time);
-	}
-
-	static class ySort implements Comparator<Item> {
-		public int compare(Item i1, Item i2) {
-			if (i1.y < i2.y) {
-				return -1;
-			} else if (i1.y == i2.y) {
-				if (i1.x < i2.x) {
-					return -1;
-				} else if (i1.x == i2.x) {
-					return 0;
-				}
-				return 1;
-			} else {
-				return 1;
 			}
 		}
 	}
 
-	static class Item {
-		int x;
-		int y;
-		int n;
-		int move;
-
-		Item(int x, int y, int move) {
-			this.x = x;
-			this.y = y;
-			this.n = this.y * N + this.x;
-			this.move = move;
-		}
+	public static boolean isBoundary(int row, int col) {
+		return (row >= 0 && row < N) && (col >= 0 && col < N);
 	}
 }
